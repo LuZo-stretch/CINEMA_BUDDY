5b46bafb8e760cdf5e029acb244cb4de9a72c3d9
Lucia Galarza <lzgalarza@gmail.com>
2024-03-03 17:30:27 +0000

    map set-up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_29_005953) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_03_051135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_29_005953) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "liked_movies", force: :cascade do |t|
