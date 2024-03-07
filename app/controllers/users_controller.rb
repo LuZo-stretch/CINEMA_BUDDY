3e3a3de77f07665176e0d0717959b49ecc52082b
angelo cabello <angelocabello99@gmail.com>
2024-03-07 20:39:17 +0100

    create profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+ 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user), notice: 'Profile created successfully!'
+    else
+      render :new
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:email, :name, :age, :description, :photo)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :age, :description, :photo)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_secure_password
   has_one_attached :photo
 
  # before_validation :skip_password_validation, on: :create
