aac74d5a0fd7c4ed749ddd371d6f227f8d3725c1
Lucia Galarza <lzgalarza@gmail.com>
2024-03-09 14:22:39 +0000

    like function from index and show

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,4 @@
 class Movie < ApplicationRecord
   has_one_attached :photo
+  acts_as_favoritable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :users
-  resources :movies, only: :show
-
+  resources :movies do
+    post 'toggle_favorite', on: :member
+  end
 end
