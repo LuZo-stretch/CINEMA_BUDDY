7fca514d1cbe12dd39f4df066e2f00b731546e0d
angelo cabello <angelocabello99@gmail.com>
2024-03-10 10:08:02 +0100

    deleted my changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
+  
   def new
     @user = User.new
   end
- 
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, :age, :description, :photo)
+    params.require(:user).permit(:email, :name, :age, :description, :photo)
   end
 end
