eca98033888a7c80a7283e73c4f58b6b74f4eec9
Anahi Herrera <anahiheru@gmail.com>
2024-03-20 19:25:26 +0100

    deleted like button

--- app/javascript/like-button.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.addEventListener('click', function(event) {
-    const likeButton = event.target.closest('.movie .like-icon button.btn.text-warning');
-    if (likeButton) {
-      likeButton.classList.toggle('like-clicked');
-    }
-  });
-});
