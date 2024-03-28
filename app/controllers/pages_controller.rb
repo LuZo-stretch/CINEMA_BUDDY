c5fa004e765c4188adcc2e48542843af88f068c4
Lucia Galarza <lzgalarza@gmail.com>
2024-03-28 00:42:52 +0000

    redoing the code, deleted navbar, edit profile and some code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
   before_action :authenticate_user!, only: [:profile]
   def home
-    @hide_navbar = true
+    # @hide_navbar = true
   end
   def profile
     @user = current_user
   end
 end
-
